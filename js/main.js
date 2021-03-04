f6d0cfde62d656a6f65c58598befded81119a367
Brandon Argel <brandargel@gmail.com>
2021-03-04 11:23:49 -0600

    Aside terminado

--- js/main.js
+++ js/main.js
@@ -41,20 +41,3 @@ window.onscroll = function () {
   }
   scroll = desplazamiento_actual;
 };
-
-// Mostrar elementos
-let animado = document.querySelectorAll(".animado");
-
-function mostrarScroll() {
-  let scrollTop = document.documentElement.scrollTop;
-  for (var i = 0; i < animado.length; i++) {
-    let alturaAnimado = animado[i].offsetTop;
-    if (alturaAnimado - 500 < scrollTop) {
-      animado[i].style.opacity = 1;
-      animado[i].classList.add("mostrarIzquierda");
-      animado[i].classList.add("mostrarDerecha");
-    }
-  }
-}
-
-window.addEventListener("scroll", mostrarScroll);
