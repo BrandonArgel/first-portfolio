6100afc8061883f317bd4df16a142550f7284110
Brandon Argel <brandargel@gmail.com>
2021-03-22 19:40:53 -0600

    Effects in the hero

--- js/main.js
+++ js/main.js
@@ -22,22 +22,24 @@ panelToggle.addEventListener("click", function () {
   }
 });
 
-let ToggleA = document.querySelector('a.a'), ToggleB = document.querySelector('a.b'), ToggleC = document.querySelector('a.c');
+let ToggleA = document.querySelector("a.a"),
+  ToggleB = document.querySelector("a.b"),
+  ToggleC = document.querySelector("a.c");
 ToggleA.addEventListener("click", aClose);
 ToggleB.addEventListener("click", aClose);
 ToggleC.addEventListener("click", aClose);
 
 function aClose() {
-  setTimeout(function(){
-  if (document.getElementById("aside").classList.contains("active")) {
-    document.getElementById("aside").classList.toggle("active");
-    document.getElementById("particles-js").classList.toggle("index");
-    document.getElementById("main").classList.toggle("opacity");
-    document
-      .getElementById("btn-toggle")
-      .classList.remove("header__menu--close-icon");
-  }
-  },100)
+  setTimeout(function () {
+    if (document.getElementById("aside").classList.contains("active")) {
+      document.getElementById("aside").classList.toggle("active");
+      document.getElementById("particles-js").classList.toggle("index");
+      document.getElementById("main").classList.toggle("opacity");
+      document
+        .getElementById("btn-toggle")
+        .classList.remove("header__menu--close-icon");
+    }
+  }, 100);
 }
 
 // Header
@@ -57,3 +59,56 @@ window.onscroll = function () {
   }
   scroll = desplazamiento_actual;
 };
+
+// Texto Animado
+
+class TextoAnimado {
+  constructor(id, objetivo) {
+    this.texto = document.getElementById(id);
+    this.objetivo = document.getElementById(objetivo);
+    this.letras = this.texto.innerText.split("");
+
+    this.texto.innerText = "";
+
+    this.letras.forEach((letra) => {
+      let caracter = letra === " " ? "&nbsp;" : letra;
+
+      this.texto.innerHTML =
+        this.texto.innerHTML +
+        `
+				<div>
+					<span>${caracter}</span>
+					<span class="segunda-linea">${caracter}</span>
+				</div>
+			`;
+    });
+
+    this.objetivo.addEventListener("mouseenter", () => {
+      let cuenta = 0;
+
+      const intervalo = setInterval(() => {
+        if (cuenta < this.texto.children.length) {
+          this.texto.children[cuenta].classList.add("animacion");
+          cuenta += 1;
+        } else {
+          clearInterval(intervalo);
+        }
+      }, 20);
+    });
+
+    this.objetivo.addEventListener("mouseleave", () => {
+      let cuenta = 0;
+
+      const intervalo = setInterval(() => {
+        if (cuenta < this.texto.children.length) {
+          this.texto.children[cuenta].classList.remove("animacion");
+          cuenta += 1;
+        } else {
+          clearInterval(intervalo);
+        }
+      }, 20);
+    });
+  }
+}
+
+new TextoAnimado("name", "main__hero");
