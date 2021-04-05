84ea2bea5c18e783a731b69fc47a9dd47ceef335
Brandon Argel <brandargel@gmail.com>
2021-04-05 02:08:04 -0500

    Animations added

---
+++ js/animations.js
@@ -0,0 +1,22 @@
+window.onload = window.addEventListener('scroll', function(){
+    function fade(direction){
+        var animate = document.querySelectorAll('.fade_'+direction);
+        for(var i = 0; i < animate.length; i++){
+            var altura = window.innerHeight/1.3;
+            var distancia = animate[i].getBoundingClientRect().top;
+            console.log(altura);
+            console.log(distancia);
+            animate[i].classList.add('transform_'+direction)
+            if(distancia <= altura){
+                animate[i].classList.add('appear');
+            } else {
+                animate[i].classList.remove('appear');
+            }
+        }
+    }
+    
+    fade('left');
+    fade('right');
+    fade('up');
+    fade('down');
+})
